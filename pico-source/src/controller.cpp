074353c7e98f13f5fe1fdc1ef8a4e4b59b45d4dc
robert <rer3378@rit.edu>
2023-04-02 21:03:08 -0700

    Added PID, fixed pwm.

--- pico-source/src/controller.cpp
+++ pico-source/src/controller.cpp
@@ -9,19 +9,49 @@
 #include "motor.h"
 
 const uint LEFT_MOTOR_PIN = 3;
-const uint RIGHT_MOTOR_PIN = 4;
+const uint LEFT_SPEED_PIN = 4;
+const uint LEFT_DIR_PIN = 5;
+//const uint RIGHT_MOTOR_PIN = ;
+
+Motor left_motor;
+Motor right_motor;
+
+static void motor_speed_isr(uint gpio, uint32_t events)
+  {
+    if (gpio == LEFT_SPEED_PIN)
+      left_motor.speed_pin_ISR();
+    //else if (gpio == RIGHT_SPEED_PIN)
+      //right_motor.speed_pin_ISR();
+  }
+
+void print_speed(Motor* motor)
+  {
+    for (int i = 0; i < 100; i++)
+      {
+        motor->compute_PID();
+        printf("Output duty cycle: %f ", motor->get_duty_cycle());
+        printf("Velocity: %f\n", motor->get_velocity());
+        sleep_ms(10);      }
+  }
 
 int main() {
 
-  Motor left_motor(LEFT_MOTOR_PIN, 4, 5);
+  left_motor.set_pins(LEFT_MOTOR_PIN, LEFT_SPEED_PIN, LEFT_DIR_PIN);
+  left_motor.init_gpio();
+  gpio_set_irq_enabled_with_callback(LEFT_SPEED_PIN,
+                                     GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+                                     true,
+                                     &motor_speed_isr);
+
+
 
   stdio_init_all();
   while(true){
     printf("Motor on\n");
-    left_motor.set_duty_cycle(0.01);
-    sleep_ms(1000);
+    left_motor.set_velocity(1000);
+    print_speed(&left_motor);
     printf("Motor off\n");
-    left_motor.set_duty_cycle(0.0);
-    sleep_ms(1000);
+    left_motor.set_velocity(0);
+    print_speed(&left_motor);
   }
 }
--- pico-source/src/motor.cpp
+++ pico-source/src/motor.cpp
@@ -1,15 +1,34 @@
 #include "motor.h"
 
+Motor::Motor()
+{
+  init_PID();
+}
+
 Motor::Motor(uint _pwm_pin, uint _speed_pin, uint _direction_pin)
+  {
+    init_PID();
+    set_pins(_pwm_pin, _speed_pin, _direction_pin);
+  }
+
+void Motor::set_velocity(float angular_vel)
+  {
+    setpoint = angular_vel;
+  }
+
+double Motor::get_velocity()
+  {
+    return velocity;
+  }
+
+void Motor::set_pins(uint _pwm_pin, uint _speed_pin, uint _direction_pin)
   {
     pwm_pin = _pwm_pin;
     speed_pin = _speed_pin;
     direction_pin = _direction_pin;
-
-    init_gpio();
   }
 
-void Motor::set_duty_cycle(double dc)
+void Motor::set_duty_cycle(float dc)
   {
     // Clip duty cycle at max allowed value
     if (dc > max_duty_cycle)
@@ -24,6 +43,7 @@ void Motor::init_gpio()
     // Configure motor pwm pin
     pwm_config cfg = pwm_get_default_config();
     pwm_config_set_wrap(&cfg, pwm_count_top);
+    pwm_config_set_clkdiv(&cfg, 256);
     pwm_init(pwm_gpio_to_slice_num(pwm_pin), &cfg, true);
 
     gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
@@ -32,8 +52,46 @@ void Motor::init_gpio()
     set_duty_cycle(0.0);
 
     // Configure motor direction pin
+  }
 
+void Motor::init_PID()
+  {
+    motorPID = std::make_unique<QuickPID>(QuickPID(&velocity, &output_duty_cycle, &setpoint,
+                                                   Kp, Ki, Kd, motorPID->Action::direct));
+    //motorPID->SetTunings(Kp, Ki, Kd);
+    //motorPID->SetOutputLimits(-max_duty_cycle, max_duty_cycle);
+    motorPID->SetOutputLimits(0, max_duty_cycle);
+    motorPID->SetMode(motorPID->Control::automatic);
+  }
 
-    // Configure motor speed pin
+float Motor::get_duty_cycle()
+  {
+    return output_duty_cycle;
+  }
+
+void Motor::compute_PID()
+  {
+    motorPID->Compute();
+    set_duty_cycle(output_duty_cycle);
+  }
+
+void Motor::speed_pin_ISR()
+  {
+    // We are here because there was a state change on the speed pin.
+    // This means the motor has spun 1/90 of a full rotation in either direction.
+    if(first_reading)
+    {
+      last_time = time_us_64();
+      first_reading = false;
+      return;
+    }
+
+    uint64_t new_time = time_us_64();
+
+    double dt = double(new_time - last_time) / 1e6;
+    velocity = (4 * direction) / dt;
+    counter += int(direction);
+
+    last_time = new_time;
 
   }
